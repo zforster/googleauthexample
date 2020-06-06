cfec186a05bfa412fff490f2f2f9f54d04a4a9ea
zforster <zak.forster16@gmail.com>
2020-06-06 23:09:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  state = {
+      clientId: 'idhere',
+      familyName: null,
+      givenName: null,
+      userToken: null,
+      imageURL: null,
+      name: null
+  };
+
+  setFamilyName = (familyName) => {
+      this.setState({familyName: familyName})
+  };
+
+  setGivenName = (givenName) => {
+      this.setState({givenName: givenName})
+  };
+
+  setUserToken = (userToken) => {
+      console.log(userToken);
+      this.setState({userToken: userToken})
+  };
+
+  setImageURL = (imageURL) => {
+      this.setState({imageURL: imageURL})
+  };
+
+  setName = (name) => {
+      this.setState({name: name})
+  };
+
+  onLoginSuccess = (response) => {
+      this.setFamilyName(response.profileObj.familyName);
+      this.setGivenName(response.profileObj.givenName);
+      this.setUserToken(response.tokenId);
+      this.setImageURL(response.profileObj.imageUrl);
+      this.setName(response.profileObj.name);
+  };
+
+  getGoogleLogin = () => {
+      return (
+          <GoogleLogin
+              onSuccess={this.onLoginSuccess}
+              onFailure={console.log("oh no")}
+              clientId={this.state.clientId}
+              buttonText={"Sign in"}
+          />
+      )
+  };
+
+  render(){
+      if(!this.state.userToken){
+          return this.getGoogleLogin();
+      }
+      return(
+          <div>
+              {this.state.familyName}
+              {this.state.givenName}
+              {this.state.userToken}
+              {this.state.imageURL}
+              {this.state.name}
+          </div>
+      )
+  }
 }
 
 export default App;
